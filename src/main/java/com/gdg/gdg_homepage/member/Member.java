9ff8a557b24aabf418855b7ab4136fdbf21f82a3
gunu <luckdaniel@naver.com>
2026-01-09 03:04:55 +0900

    feat: 프로필 도메인 엔티티 추가 및 JPA 엔티티 기본 설정 보완
    - 마이페이지 프로필 정보를 저장하기 위한 Profile 엔티티 추가
    - 기술 스택을 개별 항목으로 관리하기 위해 ProfileTechStack 엔티티 추가
    - JPA 규칙에 맞게 Member 엔티티에 기본 식별자(@Id) 및 생성자 설정 보완
    - 애플리케이션 정상 기동을 위한 엔티티 초기 설정 정리
    
    프로필 정보는 인증/계정 책임을 가지는 Member 도메인과 분리하여
    Profile 도메인으로 관리하도록 설계했습니다.
    
    이를 통해 마이페이지, 기수별 멤버 아카이브 등
    확장 가능한 구조를 확보하고,
    인증 로직과 화면 표시 로직의 책임을 명확히 나누었습니다.
    
    또한 JPA 엔티티 규칙에 맞춰 Member 엔티티의 식별자 및 생성자 설정을 보완하여
    서버가 안정적으로 기동되도록 정리했습니다.

--- src/main/java/com/gdg/gdg_homepage/member/Member.java
+++ src/main/java/com/gdg/gdg_homepage/member/Member.java
@@ -1,116 +1,123 @@
 package com.gdg.gdg_homepage.member;
 
-
 import com.gdg.gdg_homepage.common.infrastructure.BaseEntity;
 
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Member extends BaseEntity {
 
-	private final Long id;
 
-	private String nickname;
-	private String username;
-	private String tel;
-	private String email;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private boolean isDeleted;
+    private String nickname;
+    private String username;
+    private String tel;
+    private String email;
 
-	private String password;
+    private boolean isDeleted;
 
-	private String organization;
+    private String password;
 
-	private Long organizationId;
+    private String organization;
 
-	private MemberRole memberRole;
+    private Long organizationId;
 
-	private MemberType memberType;
+    private MemberRole memberRole;
 
-	//
-	// public static Member createMemberFromSignup(MemberSignupDto signupDto, String encodedPassword) {
-	// 	return Member.builder()
-	// 		.nickname(signupDto.nickname())
-	// 		.username(signupDto.username())
-	// 		.password(encodedPassword)
-	// 		.email(signupDto.email())
-	// 		.memberRole(signupDto.memberRole())
-	// 		.memberType(signupDto.memberType())
-	// 		.isMarketingAgreed(signupDto.isMarketingAgreed())
-	// 		.organization(signupDto.organization() != null ? signupDto.organization() : null)
-	// 		.organizationId(signupDto.organizationId() != null ? signupDto.organizationId() : null)
-	// 		.isDeleted(false)
-	// 		.build();
-	// }
-	//
-	// public static Member createPrinterMember(String tel, String encodedPassword) {
-	// 	return Member.builder()
-	// 		.tel(tel)
-	// 		.password(encodedPassword)
-	// 		.memberRole(MemberRole.PRINTER)
-	// 		.isDeleted(false)
-	// 		.build();
-	// }
-	//
-	// public static Member createClassroomStudent(
-	// 	String username, Classroom classroom, String password) {
-	// 	return Member.builder()
-	// 		.username(username)
-	// 		.nickname(username)
-	// 		.memberRole(MemberRole.STUDENT)
-	// 		.memberType(MemberType.CLASS)
-	// 		.isDeleted(false)
-	// 		.password(password)
-	// 		.classroom(classroom)
-	// 		.build();
-	// }
-	//
-	// public void changeNameInfo(String newName) {
-	// 	this.username = newName;
-	// 	this.nickname = newName;
-	// }
-	//
-	// public void updatePassword(String password) {
-	// 	this.password = password;
-	// }
-	//
-	// public Boolean isMemberTypeMatched(MemberType memberType) {
-	// 	return Objects.equals(this.memberType, memberType);
-	// }
-	//
-	// public Member softDelete() {
-	// 	if (!isDeleted) {
-	// 		isDeleted = true;
-	// 	}
-	// 	return this;
-	// }
-	//
-	// public Member updateMember(MemberUpdateRequestDto request) {
-	// 	if (request.nickname() != null) {
-	// 		this.nickname = request.nickname();
-	// 	}
-	// 	if (request.username() != null) {
-	// 		this.username = request.username();
-	// 	}
-	// 	if (request.tel() != null) {
-	// 		this.tel = request.tel();
-	// 	}
-	// 	if (request.email() != null) {
-	// 		this.email = request.email();
-	// 	}
-	// 	if (request.isMarketingAgreed() != null) {
-	// 		this.isMarketingAgreed = request.isMarketingAgreed();
-	// 	}
-	// 	return this;
-	// }
-	//
-	// public Member updateMemberRole(MemberRole memberRole) {
-	// 	this.memberRole = memberRole;
-	// 	return this;
-	// }
+    private MemberType memberType;
+
+    //
+    // public static Member createMemberFromSignup(MemberSignupDto signupDto, String encodedPassword) {
+    // 	return Member.builder()
+    // 		.nickname(signupDto.nickname())
+    // 		.username(signupDto.username())
+    // 		.password(encodedPassword)
+    // 		.email(signupDto.email())
+    // 		.memberRole(signupDto.memberRole())
+    // 		.memberType(signupDto.memberType())
+    // 		.isMarketingAgreed(signupDto.isMarketingAgreed())
+    // 		.organization(signupDto.organization() != null ? signupDto.organization() : null)
+    // 		.organizationId(signupDto.organizationId() != null ? signupDto.organizationId() : null)
+    // 		.isDeleted(false)
+    // 		.build();
+    // }
+    //
+    // public static Member createPrinterMember(String tel, String encodedPassword) {
+    // 	return Member.builder()
+    // 		.tel(tel)
+    // 		.password(encodedPassword)
+    // 		.memberRole(MemberRole.PRINTER)
+    // 		.isDeleted(false)
+    // 		.build();
+    // }
+    //
+    // public static Member createClassroomStudent(
+    // 	String username, Classroom classroom, String password) {
+    // 	return Member.builder()
+    // 		.username(username)
+    // 		.nickname(username)
+    // 		.memberRole(MemberRole.STUDENT)
+    // 		.memberType(MemberType.CLASS)
+    // 		.isDeleted(false)
+    // 		.password(password)
+    // 		.classroom(classroom)
+    // 		.build();
+    // }
+    //
+    // public void changeNameInfo(String newName) {
+    // 	this.username = newName;
+    // 	this.nickname = newName;
+    // }
+    //
+    // public void updatePassword(String password) {
+    // 	this.password = password;
+    // }
+    //
+    // public Boolean isMemberTypeMatched(MemberType memberType) {
+    // 	return Objects.equals(this.memberType, memberType);
+    // }
+    //
+    // public Member softDelete() {
+    // 	if (!isDeleted) {
+    // 		isDeleted = true;
+    // 	}
+    // 	return this;
+    // }
+    //
+    // public Member updateMember(MemberUpdateRequestDto request) {
+    // 	if (request.nickname() != null) {
+    // 		this.nickname = request.nickname();
+    // 	}
+    // 	if (request.username() != null) {
+    // 		this.username = request.username();
+    // 	}
+    // 	if (request.tel() != null) {
+    // 		this.tel = request.tel();
+    // 	}
+    // 	if (request.email() != null) {
+    // 		this.email = request.email();
+    // 	}
+    // 	if (request.isMarketingAgreed() != null) {
+    // 		this.isMarketingAgreed = request.isMarketingAgreed();
+    // 	}
+    // 	return this;
+    // }
+    //
+    // public Member updateMemberRole(MemberRole memberRole) {
+    // 	this.memberRole = memberRole;
+    // 	return this;
+    // }
 }
